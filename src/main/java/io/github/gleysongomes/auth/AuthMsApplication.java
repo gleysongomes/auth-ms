755feef8e8c5cc7f60f160de9aff15823414bc5e
Gleyson Gomes <fgleysondasilva@gmail.com>
2022-10-23 13:26:03 -0300

    Microservice auth.

---
+++ src/main/java/io/github/gleysongomes/auth/AuthMsApplication.java
@@ -0,0 +1,15 @@
+package io.github.gleysongomes.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gleysongomes/auth/AuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gleysongomes.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
